f1b7ed31d80e7b436c488b23a55cd48b801f1e55
RossDan96 <rossda@oregonstate.edu>
2017-05-03 20:54:36 -0700

    Dining philosophers done...probably

--- concurrency2.c
+++ concurrency2.c
@@ -12,7 +12,7 @@
 #include <time.h>
 #include "mt19937ar.h"
 
-pthread_mutex_t forks[5] = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t forks[5];
 
 unsigned int randomval(){
         unsigned int value;
@@ -49,7 +49,7 @@ unsigned int randomval(){
 unsigned int randomminmax(unsigned int min, unsigned int max){
         unsigned int value;
         if(min > max){
-                printf("IMPROPER RAND ARGS");
+                printf("IMPROPER RAND ARGS \n");
                 value = 0;
                 return value;
         }
@@ -76,7 +76,7 @@ int identify(char* name){
 		id = 4;
 		return id;
 	}else{
-		printf("IMPROPER THREAD ID");
+		printf("IMPROPER THREAD ID \n");
 		return id;
 	}
 }
@@ -84,7 +84,7 @@ void think(char* IDvar){
 	unsigned int think_time = 0;
 	int id = identify(IDvar);
 	think_time = randomminmax(1,20);
-	printf("%s thinks for %u",IDvar,think_time);
+	printf("%s thinks for %u \n",IDvar,think_time);
 	sleep(think_time);
 
 }
@@ -92,76 +92,85 @@ void eat(char* IDvar){
 	unsigned int eat_time = 0;
 	int id = identify(IDvar);
 	eat_time = randomminmax(2,9);
-	printf("%s eats for %u",IDvar,eat_time);
+	printf("%s eats for %u \n",IDvar,eat_time);
 	sleep(eat_time);
 
 }
 void getforks(char* IDvar){
 	int id = identify(IDvar);
-	pthread_mutex_lock(&forks[id]);
-	printf("%s picks up fork %d",IDvar,id);
 	int secondfork = ((id+1)%5);
+	pthread_mutex_trylock(&forks[secondfork]);
+	pthread_mutex_lock(&forks[id]);
+	pthread_mutex_unlock(&forks[secondfork]);
+	printf("%s picks up fork %d \n",IDvar,id);
 	pthread_mutex_lock(&forks[secondfork]);
-	printf("%s picks up fork %d",IDvar,secondfork);
+	printf("%s picks up fork %d \n",IDvar,secondfork);
 }
 void putforks(char* IDvar){
 	int id = identify(IDvar);
 	pthread_mutex_unlock(&forks[id]);
+	printf("%s puts down fork %d \n",IDvar,id);
 	int secondfork = ((id+1)%5);
 	pthread_mutex_unlock(&forks[secondfork]);
+	printf("%s puts down fork %d \n",IDvar,secondfork);
 }
 
 void *philosphize(void *IDvar){
 	int alive = 1;
-	printf("IDvar %s",IDvar);
-	while(alive){
+	int count = 0;
+	printf("IDvar %s \n",IDvar);
+	while(alive == 1){
 		think(IDvar);
 		getforks(IDvar);
 		eat(IDvar);
-		putforks(IDvar);	
+		putforks(IDvar);
+		if(count > 100){
+				alive = 0;
+		}
+		count++;
 	}
-	
 }
 
 int main(int argc, char **argv){
-		printf("check actual 0");
-        if(argc != 1){
+		printf("\n  check actual 0");
+        if(argc > 1){
                 printf("Incorrect number of arguments");
                 return 1;
         }
         int i,j;
         time_t t=time(0);
 		printf("check 0");
-        for(i=0; i<5; i++)
+        for(i=0; i<5; i++){
 			pthread_mutex_init(&(forks[i]), NULL);
-        printf("check 1");
+		}
+        printf("check 1 \n");
 		pthread_t philosophers[5];
 		char* name1 = "Aristotle";
 		char* name2 = "Plato";
 		char* name3 = "Socrates";
 		char* name4 = "Decartes";
 		char* name5 = "Confucius";
-		printf("check 2");
+		printf("check 2 \n");
         // Create five threads
         pthread_create(&(philosophers[0]), NULL, philosphize, (void *) name1);
-		printf("check 3");
+		printf("check 3 \n");
 		pthread_create(&(philosophers[2]), NULL, philosphize, (void*) name2);
-		printf("check 4");
+		printf("check 4 \n");
         pthread_create(&(philosophers[1]), NULL, philosphize, (void*) name3);
-		printf("check 5");
+		printf("check 5 \n");
 	    pthread_create(&(philosophers[3]), NULL, philosphize, (void*) name4);
-		printf("check 6");
+		printf("check 6 \n");
 	    pthread_create(&(philosophers[4]), NULL, philosphize, (void*) name5);
-		printf("check 7");
+		printf("check 7 \n");
 		for(j=0; j<5; j++)
 			pthread_join(philosophers[j],NULL);
-		printf("check 8");
+		printf("check 8 \n");
 		int million = 1;
 		int	count = 0;
         while(million){
-			printf("check 9");
+			
 			count++;
-			if(count > 10000000){
+			if(count > 1000000000000000000){
 				million =0;
 			}
 		}
